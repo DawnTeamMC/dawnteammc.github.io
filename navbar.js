8f7490b6ccdb986da0bc713251330cdff73fc62b
Hugman <contact.hugman@gmail.com>
2022-04-16 17:06:18 +0200

    Top navbar and major updates to CSS

--- navbar.js
+++ navbar.js
@@ -1,13 +1,12 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+document.querySelector("#sideMenu\\:openBtn").addEventListener("click", openSideMenu);
+document.querySelector("#sideMenu\\:closeBtn").addEventListener("click", closeSideMenu);
+
+function openSideMenu() {
+    document.querySelector("#sideMenu").classList.add("open")
+    document.querySelector("#main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+function closeSideMenu() {
+    document.querySelector("#sideMenu").classList.remove("open");
+    document.querySelector("#main").style.marginLeft = "0";
 }
